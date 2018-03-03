92e20688c2f07c6796e4ae85fe5a2fe019fefdea
Saydul Bashar <sbashar@idirect.net>
2018-03-03 23:36:56 +0000

    Initial commit

---
+++ InsertionSortDecisionTree.py
@@ -0,0 +1,20 @@
+import itertools;
+
+threeElementPermutation = list(itertools.permutations([3,2,1]))
+
+for elementList in threeElementPermutation:
+    elementList = list(elementList)
+    print(elementList)
+    for i in range(1,len(elementList)):
+        j = i
+        while j > 0:
+            if elementList[j-1] <= elementList[j]:
+                print("a" + str(j) + " <= a" + str(j+1))
+                temp = elementList[j]
+                elementList[j] = elementList[j-1]
+                elementList[j-1] = temp
+                j = j - 1
+            else:
+                print("a" + str(j) + " > a" + str(j+1))
+                break
+    print(elementList)
