d66ea44f9af62fc5c56c00a0f5b8e0e71a67cc9f
Saydul Bashar <sbashar@idirect.net>
2018-03-04 15:01:40 +0000

    Added more logic to print out the sorted list

--- DetailedThreeElementSortingDecisionTree.py
+++ DetailedThreeElementSortingDecisionTree.py
@@ -1,9 +1,9 @@
 import itertools
 
-threeElementPermutation = list(itertools.permutations(["a1","a2","a3"]))
+threeElementPermutation = list(itertools.permutations([1,2,3]))
 
-for elementList in threeElementPermutation:
-    a1,a2,a3 = elementList
+for a1,a2,a3 in threeElementPermutation:
+    print("Input sequence: a1 = " + str(a1) + " a2 = " + str(a2) + " a3 = " + str(a3))
     if a3 > a2:
         print("a3 > a2")
         if a1 > a2:
@@ -12,16 +12,19 @@ for elementList in threeElementPermutation:
                 print("a1 > a3")
             else:
                 print("a1 <= a3")
+                print("Sorted sequence: a2 = " + str(a2) + " <= a1 = " + str(a1) + " <= a3 = " + str(a3))
         else:
             print("a1 <= a2")
+            print("Sorted sequence: a1 = " + str(a1) + " <= a2 = " + str(a2) + " <= a3 = " + str(a3))
     else:
         print("a3 <= a2")
         if a1 > a2:
             print("a1 > a2")
+            print("Sorted sequence: a3 = " + str(a3) + " <= a2 = " + str(a2) + " <= a1 = " + str(a1))
         else:
             print("a1 <= a2")
             if a3 > a1:
                 print("a3 > a1")
+                print("Sorted sequence: a1 = " + str(a1) + " <= a3 = " + str(a3) + " <= a2 = " + str(a2))
             else:
                 print("a3 <= a1")
-    print(elementList)
