6b1c7534e00e3185ae7f342e8fe3e0222ff3672c
Saydul Bashar <sbashar@idirect.net>
2018-03-04 13:11:43 +0000

    Completed the insertion sort logic and also added three element decision tree.

--- InsertionSortDecisionTree.py
+++ InsertionSortDecisionTree.py
@@ -1,6 +1,6 @@
-import itertools;
+import itertools
 
-threeElementPermutation = list(itertools.permutations([3,2,1]))
+threeElementPermutation = list(itertools.permutations([1,2,3,4]))
 
 for elementList in threeElementPermutation:
     elementList = list(elementList)
@@ -8,13 +8,12 @@ for elementList in threeElementPermutation:
     for i in range(1,len(elementList)):
         j = i
         while j > 0:
-            if elementList[j-1] <= elementList[j]:
-                print("a" + str(j) + " <= a" + str(j+1))
+            if elementList[j-1] > elementList[j]:
+                print("a" + str(j) + " > a" + str(j+1))
                 temp = elementList[j]
                 elementList[j] = elementList[j-1]
                 elementList[j-1] = temp
                 j = j - 1
             else:
-                print("a" + str(j) + " > a" + str(j+1))
+                print("a" + str(j) + " <= a" + str(j+1))
                 break
-    print(elementList)
