b9213faf0d9210dcbce7af7172925aaee914928b
Saydul Bashar <sbashar@idirect.net>
2018-03-04 22:28:16 +0000

    Completed the insertion sort decision tree logic with detailed logging.

--- InsertionSortDecisionTree.py
+++ InsertionSortDecisionTree.py
@@ -1,26 +1,19 @@
 import itertools
-
-elementPermutation = list(itertools.permutations([1, 2, 3]))
-
+elementPermutation = list(itertools.permutations(["a1", "a2", "a3", "a4"]))
+print("Available leaves: " + str(len(elementPermutation)))
 for elementList in elementPermutation:
     elementList = list(elementList)
-    inputSequence = "Input sequence:"
-    sortedSequence = "Sorted sequence:"
-    for i in range(len(elementList)):
-        inputSequence += " a" + str(i+1) + " = " + str(elementList[i])
-    print(inputSequence)
+    print("Input : " + str(elementList))
     for i in range(1,len(elementList)):
         j = i
         while j > 0:
             if elementList[j-1] > elementList[j]:
-                print("a" + str(j) + " > a" + str(j+1))
+                print(str(j) + " > " + str(j+1))
                 temp = elementList[j]
                 elementList[j] = elementList[j-1]
                 elementList[j-1] = temp
                 j = j - 1
             else:
-                print("a" + str(j) + " <= a" + str(j+1))
+                print(str(j) + " <= " + str(j+1))
                 break
-    for i in range(len(elementList)):
-        sortedSequence += " a" + str(i+1) + " = " + str(elementList[i])
-    print(sortedSequence)
+    print("Sorted : " + str(elementList))
